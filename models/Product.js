6bfa9946007f6d1db628a6d12c3965e8427520f1
Jank Jank <jank.jank1812@gmail.com>
2021-06-23 20:56:36 -0700

    CLasses done. Starting Associations.

--- models/Product.js
+++ models/Product.js
@@ -20,31 +20,34 @@ Product.init(
     product_name: {
       type: DataTypes.STRING,
       allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
     },
   
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false, 
+      validate: {
+        isDecimal: true,
+      },
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    }
   },
+
   {
     sequelize,
     timestamps: false,
